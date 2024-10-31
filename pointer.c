a81e0f55ab059613c1b2c4d2bdb4c26f9faaba81
mahade <mahade.adib45@gmail.com>
2024-10-31 21:14:48 +0600

    week 4 problems

---
+++ pointer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int a = 5, b = 6;
+
+    int *x = &a, *y = &b;
+
+    printf("%d %d\n", a, b);
+
+    int temp = *x;
+
+    *x = *y;
+
+    *y = temp;
+
+    printf("%d %d\n", a, b);
+}
